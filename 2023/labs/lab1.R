c15fc385f7c1dc1288272692b85acdc00b835978
rossanatorres <rossanatorresalvarez@gmail.com>
2024-07-21 05:07:03 -0700

    2024 update

---
+++ 2023/labs/lab1.R
@@ -0,0 +1,273 @@
+
+# Definir variables =====================================================
+
+#  X es el valor asignado a x
+# escoger un nombre adecuado
+
+val1 <- 1
+val1 = 2 # equivalente
+
+# Variable vacia
+vacia <- NA
+
+class(vacia)
+
+# Valor logico
+
+logico <- TRUE
+logico <- FALSE
+logico <- T
+logico <- F
+
+# Reescribir variable
+# cuidado!!!
+
+
+# Funcion class
+
+
+# Ver que hay dentro de variable
+
+# opcion 1
+val2 <- 2
+val2
+
+# opcion 2
+(val2 <- 2) # cuidado si no cerramos nos sale error
+#(val2 <- 2 usar esc para evitar quedarnos atorados en el codigo
+
+# opcion 3
+print(val2)
+
+
+# Hacer operaciones aritmeticas
+
+val3 <- val1 + val2
+
+val4 <- val3 + 2
+
+# Hacer operacion logica
+
+val4 > 8
+val4 < 8
+val4 <= 8
+val4 >= 8
+val4 == 6
+
+# Vectores =====================================================
+
+# Crear vector
+#1. Numericos
+mivector1 <- c(1, 2, 3)
+class(mivector1)
+
+# Dimension de un vector
+# funcion length, ver ayuda
+length(mivector1)
+
+# Operaciones con vectores
+# aritmeticas
+
+mivector1 + 1
+
+mivector2 <- c(4,5,6)
+
+mivector1 + mivector2
+
+
+# que observan?
+mivector1
+mivector2
+mivector1 + mivector2
+# logicas
+
+
+# class
+class(mivector1)
+
+# Asignar arreglo de valores 
+
+# a:b es para establecer un rango de a a b
+x <- 1:4 
+x
+
+
+
+y <- rep(x = 7, times = 4) # Crear un vector que con tiene 4 7s
+y
+
+
+# Secuencias
+a <- seq(from = 10, to = 23, by = 1) # contar de 10 to a cada 1 unidad
+a
+b <- seq(from = 10, to = 23) # equivalente
+b
+
+d <- seq(10, 23) # equivalente
+
+d
+
+
+# Vector vacio
+vec_vacio <- c()
+
+
+# 2. Caracteres 
+
+# Nombres de perritos mexicanos
+
+perritos <- c("Copito", 'Firulais')
+class(perritos)
+
+apellidos <- c("Torres", "Alvarez")
+
+
+# operacion (?) con caracteres
+
+nombres <- paste0(perritos, " ",apellidos)
+
+resultado <- c(nombres, mivector)
+
+
+# Indexar vectores 
+
+z <- x + y
+
+length(z)# parentesis redondos son para funciones
+
+z[3] # parentesis cuadrados son para indices
+z[2]
+z[1:3] # 
+
+
+
+# Matrices ===============================================================
+
+mimatriz <- matrix(1, nrow = 4, ncol = 3)
+
+
+# Accesar elementos
+# estructura: matrix[filas,columnas]
+# OJO: mismo numero de filas entre columnas
+mimatriz[1,1] # elemento en fila 1, columna 1
+mimatriz[1,1:3] # primera fila, todas las columnas
+mimatriz[1,] # primera fila todas las columnas
+mimatriz[,2]
+# Dimension de matriz
+dim(mimatriz)
+
+
+
+# Nombrar columnas de matriz
+colnames(mimatriz) <- c("Columna1", "Columna2", "Columna3")
+mimatriz
+rownames(mimatriz) <- c("Fila1", "Fila2", "Fila3", "Fila4")
+mimatriz
+
+mimatriz["Fila2","Columna3"]
+
+# Dataframes  =============================================================
+
+# Crear un dataframe
+df <- data.frame(x, y, z)
+
+# OJO: mismo numero de filas entre columnas (vectores)
+
+df # Ver que hay dentro del dataframe
+df$x # Usa $ para acceder columnas
+df$y
+
+# Dimension de dataframe
+
+dim(df)
+
+# Nombrar columnas dataframe
+
+colnames(df) <- c("Columna1", "Columna2", "Columna3")
+df
+# Alternativa:
+df <- data.frame("columna1"= x, "columna2" = y, "columna3" =z)
+df
+
+# Obtener nombres de dataframe
+colnames(df)
+
+# Indexar dataframes
+# estructura: df[filas,columnas]
+df[3, 1] # fila 3, columna 1
+df[4, ]  # fila 4, todas las columnas
+df[, 1]
+
+df[c(1, 2, 3), ] # Seleccionar mas de una fila
+df[1:3, ] # Seleccionar mas de una fila
+
+# pregunta que quiero contestar
+df[, 1] > 2
+
+df[df[, 1] > 2, ] # Operacion logica en fila
+
+# Que notan?
+
+
+
+
+
+# Hacer calculos/aplicar funciones
+sum(df[, 1], na.rm = T)
+sum(x)
+
+
+# Reasignar valores
+df
+df[3, 2] # un elemento: fil 3 columna 2
+df[3, 2] <- 5 
+df[3, 2] <- c(5,2) # Para reasignar me tengo que fijar en la dimension!!!!
+dim(df[3, ])
+df[3, ] <- c(1,1,1) # Reasignar con misma dimension
+
+
+
+
+df
+
+
+# Ordenar valores de columnas
+
+
+sort(x, decreasing = F) # valor default
+sort(x, decreasing = T) # cambiar default
+rev(x)
+
+micaracter <- c("Bajo", "Medio", "Alto", "Muy alto")
+
+micaracter <- sort(micaracter, decreasing = T)
+
+# Crear factor para ordenar jerarquicamente 
+mifactor <- factor(micaracter, levels = c("Bajo", "Medio", "Alto", "Muy alto"))
+class(mifactor)
+
+df2 <- cbind.data.frame(df, "columna4" = mifactor)
+row <- data.frame("columna1"=1, "columna2"=1, "columna3"=1, "columna4" =1)
+df3 <- rbind.data.frame(df, row)
+
+
+# Crear listas =============================================================
+# Arreglo de objetos de cualquier tamano
+milista <- list(1:10, 1:20, df2)
+names(milista) <- c("vector_uno", "vector_dos","data_frame")
+
+
+# Accesar listas
+# Checar dimension de lista = numbero de elementos
+length(milista)
+
+milista[[1]] #accesar al primer elemento de la lista
+milista$vector_dos # accesar por nombre
+milista[["vector_uno"]][10] #accesar al elemento 10 del objeto 1
+milista$data_frame$columna1[1:3] #accesar al data frame, columna1 y los primeros 3 elementos de la columna
+
+#
+rm(val4) # borrar algun elemento
+ls() # ver el nombre detodos los elementos que tenemos en el environment
+rm(list=ls()) # remover todo
+
---
+++ 2023/labs/lab2.5.R
@@ -0,0 +1,108 @@
+rm(list = ls())
+library(tidyverse)
+#library()
+
+# Donde yo tengo los archivos que estare usando
+setwd("~/Documents/GitHub/Rbasico")
+
+# Verificar
+#getwd() 
+
+# Abrir base FFQ  ============================================
+
+# Yo tengo la base guardada aqui
+FFQ <- read_csv("~/Documents/GitHub/Rbasico/files/ffq_long.csv")
+
+#FFQ <- FFQ <- read_csv("tu carpeta/ffq_long.csv")
+
+glimpse(FFQ)
+
+
+# Ventaja de usar pipes
+
+FFQ <- FFQ %>% 
+       select(identifier, sexo, edadanos, region, sexo, edadanos,
+              alimento, consumo) %>% 
+       mutate(edad_entero = round(edadanos)) #%>% 
+       #filter(sexo == "masculino")
+
+
+# seleccionar
+# filtrar
+# crear nueva variable (edad en enteros)
+
+# Un analisis sencillo
+
+# consumo de ssb por tipo de bebidas
+
+tipobebidas <- FFQ %>% 
+               group_by(alimento) %>% 
+               summarise(ssb_tot = sum(consumo))
+
+
+# consumo de ssb por region
+
+region <- FFQ %>% 
+  group_by(region) %>% 
+  summarise(ssb_tot = sum(consumo))
+
+
+unique(FFQ$region)
+
+
+
+# Analisis por individuo
+nrow(FFQ)
+length(unique(FFQ$identifier))
+
+ssb_unique1 <- FFQ %>% 
+  group_by(identifier) %>% 
+  summarise(ssb_consumo = sum(consumo, na.rm = TRUE))
+
+# Opcion 1 hacer merge
+
+# Opcion 2
+ssb_notunique <- FFQ %>% 
+       group_by(identifier) %>% 
+       mutate(ssb_consumo = sum(consumo, na.rm = TRUE))
+
+ssb_unique2 <- ssb_notunique%>% 
+  distinct(identifier, .keep_all=TRUE)
+
+
+# Transformar data de long a wide
+
+wide <- FFQ %>% 
+        pivot_wider(names_from = alimento, values_from = consumo)
+        
+
+
+# Abrir base Antropometria ==========================================
+
+# Yo tengo guardada la base aqui:
+
+Antropometria <- read_dta("files/Antropometria.dta")
+
+#Antropometria <- read_dta("tus carpeta/Antropometria.dta")
+
+
+# Merge por identificador
+FFQ$identifier[1]
+
+
+Antropometria$identifier <- factor(paste0("folio_",
+                                          Antropometria$folio,
+                                          "__intp_",
+                                          Antropometria$intp))
+
+antropometria_mini <- select(Antropometria, identifier, peso, talla)
+
+
+# Merge
+base_completa2 <- left_join(ssb_unique2, antropometria_mini, 
+                            by = "identifier")
+
+
+
+
+
